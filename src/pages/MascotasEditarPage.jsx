d49fb9993c1ff587c5b6b4476f8702099da464a2
Christian Mollo <christian.mollo@inacapmail.cl>
2026-07-27 01:24:44 -0400

    Se implementa la opción de editar los datos de la mascota

--- src/pages/MascotasEditarPage.jsx
+++ src/pages/MascotasEditarPage.jsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import mascotasApi from "../api/api";
 
 function MascotasEditarPage() {
 
     const {id} = useParams();
+    const navigate = useNavigate();
 
     const [mascota, setMascota] = useState(null);
     const [fetchError, setFetchError] = useState(false);
+    const [form, setForm] = useState(null);
+    const [estados, setEstados] = useState([]);
+
 
     const fetchMascota = async () => {
         try {
@@ -17,6 +21,10 @@ function MascotasEditarPage() {
             console.log(response.data);
 
             setMascota(response.data);
+            setForm(response.data);
+
+            const choices = await mascotasApi.get("choices/")
+            setEstados(choices.data.estado);
 
         } catch (error) {
 
@@ -30,6 +38,57 @@ function MascotasEditarPage() {
     useEffect(() => {
         fetchMascota();
     }, []);
+
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const handleSubmit = async (e) => {
+
+        e.preventDefault();
+
+        try {
+
+                await mascotasApi.patch(
+                    `mascotas/${id}/`,
+                    {
+                        nombre: form.nombre,
+                        descripcion: form.descripcion,
+                        edad: form.edad,
+                        estado: form.estado
+                    }
+                );
+
+                alert("Mascota actualizada correctamente");
+
+                navigate("/mascotas");
+
+        } catch (error) {
+
+        console.log(error.response?.status);
+        console.log(error.response?.data);
+        console.log(error.message);
+
+        if (error.response?.status === 400) {
+
+                    alert("Los datos ingresados no son válidos.");
+
+                } else if (error.response?.status === 404) {
+
+                    alert("La mascota no fue encontrada.");
+
+                } else {
+
+                    alert("Ha ocurrido un error al actualizar la mascota.");
+
+                }
+
+        }
+
+    }
 
 
     return (
@@ -60,19 +119,106 @@ function MascotasEditarPage() {
 
             ) : (
 
-                mascota && (
+                form && (
 
-                    <div className="card shadow p-4">
+                    <div className="card shadow">
 
-                        <h3>{mascota.nombre}</h3>
+                        <div className="card-header bg-success text-dark text-center">
+                            <h3>Datos</h3>
+                        </div>
 
-                        <p>{mascota.descripcion}</p>
+                        <div className="card-body">
+                            <form onSubmit={handleSubmit}>
 
-                        <p>Edad: {mascota.edad}</p>
+                            <div className="text-center mb-4">
 
-                        <p>Estado: {mascota.estado}</p>
+                                <img
+                                    src={form.imagen}
+                                    alt={form.nombre}
+                                    className="img-fluid rounded shadow"
+                                    style={{
+                                        maxHeight: "300px",
+                                        objectFit: "contain"
+                                    }}
+                                />
 
-                    </div>
+                            </div>
+
+                            <div className="mb-3">
+
+                                <label className="form-label">
+                                    Nombre
+                                </label>
+
+                                <input type="text" className="form-control" name="nombre" value={form.nombre} onChange={handleChange}/>
+
+                        </div>
+
+                        <div className="mb-3">
+                            
+                            <label className="form-label">
+                                Descripción
+                            </label>
+
+                            <textarea className="form-control" row="3" name="descripcion" value={form.descripcion} onChange={handleChange}/>
+
+                        </div>
+
+                        <div className="mb-3">
+
+                            <label className="form-label">
+                                Edad
+                            </label>
+
+                            <input type="number" className="form-control" name="edad" value={form.edad} onChange={handleChange}/>
+
+                        </div>
+
+                        <div className="mb-3">
+
+                            <label className="form-label">
+                                Estado
+                            </label>
+
+                            
+
+                            <select
+                                className="form-select"
+                                name="estado"
+                                value={form.estado}
+                                onChange={handleChange}
+                            >
+
+                                {
+                                    estados.map((estado) => (
+
+                                        <option
+                                            key={estado.value}
+                                            value={estado.value}
+                                        >
+                                            {estado.label}
+                                        </option>
+
+                                    ))
+                                }
+
+                            </select>
+
+                        </div>
+
+                        <div className="d-grid gap-2 mt-4">
+
+                            <button type="submit" className="btn btn-success">
+                                Guardar Cambios
+                            </button>
+
+                        </div>
+
+                        </form>
+
+                        </div>
+
+                        </div>
 
                 )
 
